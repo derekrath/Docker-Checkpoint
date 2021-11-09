12936567e43f7589177213cb7165732cf2c00e9e
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-11-09 17:58:49 -0500

    change backend port to 3000

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 3000
 const knex = require('knex')(require('./knexfile.js')[process.env.NODE_ENV || 'development']);
 const cors = require('cors')
 
