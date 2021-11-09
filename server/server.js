416d689bca7f9b05dd8a129bc59a3c179e5816ea
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-11-09 17:33:08 -0500

    added patch, post, delete

--- server/server.js
+++ server/server.js
@@ -4,14 +4,51 @@ const port = 3001
 const knex = require('knex')(require('./knexfile.js')[process.env.NODE_ENV || 'development']);
 const cors = require('cors')
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', async (req, res) => {
     console.log('Getting from /')
     const result = await knex('greetings')
         .select('*');
+    res.status(200).json(result);
+})
+
+app.post('/', async (req, res) => {
+    console.log('Posting from /')
+    let data = req.body;
+
+    let result = await knex('greetings')
+        .returning('*')
+        .insert(data)
+        .then(data => data);
 
     res.status(200).json(result);
+
+})
+
+app.patch('/:id', async (req, res) => {
+    console.log('Patching from /')
+    let id = parseInt(req.params.id)
+    const newText = req.body;
+    knex('greetings')
+        .returning('*')
+        .update({ text: newText })
+        .where({ id })
+        .then(result => res.json(result))
+})
+
+app.delete('/:id', async (req, res) => {
+    console.log('Deleting from /')
+
+    let id = parseInt(req.params.id)
+
+    knex('greetings')
+        .returning('*')
+        .where({ id })
+        .del()
+        .then(result => res.json(result))
 })
 
 app.listen(port, () => {
